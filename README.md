bc0f640bd2eccc1389c0dd572305dfa57a0660a3
Kyle M Hall <kyle@bywatersolutions.com>
2024-07-15 11:39:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# slack-update-topic
+
+A small utility to alternate slack channel topics between weeks.
+
+## Authors
+
+Contributors names and contact info
+
+[@kylemhall](https://github.com/kylemhall)
+
+## License
+
+This project is licensed under the GPL v3 License - see the LICENSE.md file for details
---
+++ slack_update_topic.py
@@ -0,0 +1,27 @@
+from slack_bolt import App
+import os
+import datetime
+import math
+
+def get_channel_id(self, channel_name):
+    for result in self.client.conversations_list():
+        for channel in result['channels']:
+            if channel['name'] == channel_name:
+                return channel['id']
+    return None
+
+weeks = math.floor( ( (datetime.datetime.utcnow() - datetime.datetime(2024,1,1)).days / 7 ) )
+
+topic = ""
+if weeks % 2 == 0:
+    topic = os.environ.get("SLACK_TOPIC_EVEN")
+else:
+    topic = os.environ.get("SLACK_TOPIC_ODD")
+
+# Initializes your app with your bot token and socket mode handler
+slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
+slack_channel_name = os.environ.get("SLACK_CHANNEL_NAME") if os.environ.get("SLACK_CHANNEL_NAME") else "dev"
+app = App(token=slack_bot_token)
+
+channel_id = get_channel_id( app, slack_channel_name )
+app.client.conversations_setTopic(channel=channel_id, topic=topic)
